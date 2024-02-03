d54345539448827cb92ff77661d7d40f70a746e7
qwertyNippon <orlandohenry100@gmail.com>
2024-02-03 11:43:07 -0500

    update

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import redis
 
 basedir = os.path.abspath(os.path.dirname(__name__))
 
@@ -9,3 +10,9 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_NOTIFICATIONS=False
     STRIPE_KEY = os.environ.get('STRIPE_KEY')
+
+    # SQLALCHEMY_ECHO = True
+    # SESSION_TYPE = "redis"
+    # SESSION_PERMANENT = False
+    # SESSION_USE_SIGNER = True
+    # SESSION_REDIS = redis.from_url("redis://127.0.0.1:5000")
