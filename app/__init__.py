529a4c1dd810f9490a3639f87bd306abfb6c587a
qwertyNippon <orlandohenry100@gmail.com>
2024-04-27 22:23:00 -0400

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -11,8 +11,30 @@ from flask_moment import Moment
 # from .payments.routes import payments
 from flask_cors import CORS
 
+from flask import Flask, render_template, redirect
+from flask_socketio import SocketIO, join_room, leave_room
+import uuid
+
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app)
+
+# ****************
+
+@app.route('/')
+def index():
+    # Redirect to a unique room ID
+    return redirect('/' + str(uuid.uuid4()))
+
+@app.route('/some_other_route')
+def some_other_route():
+    # Implement the logic for this route
+    return "This is some other route"
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+# ****************
 
 app.config.from_object(Config)
 
