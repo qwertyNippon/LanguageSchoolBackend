f4b3f5b46ad9068d157f8b8a3fa72f9db80c7911
qwertyNippon <orlandohenry100@gmail.com>
2024-06-03 17:56:23 -0400

    update

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,59 +1,38 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+# auth/routes.py
+from flask import Blueprint, request, redirect, url_for, flash, jsonify
 from .forms import RegisterForm, LoginForm
 from ..models import User
-from flask_login import current_user, login_user, logout_user
+from flask_login import login_user, logout_user
 from werkzeug.security import check_password_hash
-
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
-@auth.route('/register', methods=['GET', 'POST'])
+@auth.route('/register', methods=['POST'])
 def register():
-    form = RegisterForm()
-    print(request.method)
-    if request.method == 'POST':
-        if form.validate():
-            username = form.username.data
-            email = form.email.data
-            password = form.password.data
-            print(username, email, password)
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
 
-            user = User(username, email, password)
-            user.save_user()
+    user = User(username, email, password)
+    user.save_user()
 
-            flash('You\'re one of us now, well done!', 'success')
-            return redirect(url_for('auth.login'))
+    return jsonify({"message": "Registration successful"}), 200
 
-    return render_template('register.html', form=form)
-
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/login', methods=['POST'])
 def login():
-    form = LoginForm()
-    if request.method == 'POST':
-        if form.validate():
-            user_name = form.username.data
-            password = form.password.data
-            # print(user_name, password)
+    data = request.get_json()
+    user_name = data.get('username')
+    password = data.get('password')
 
-            user = User.query.filter_by(username=user_name).first()
-            if user:
-                print(user.password)
-                if check_password_hash(user.password, password):
-                # if user.password == password:  ---> Old way, NOT secure!
-                    flash('sweet, you\'re logged in!', 'success')
-                    login_user(user)
-                    return redirect(url_for('ig.feed'))
-                else:
-                    flash('WRONG pass!', 'danger')
-            else:
-                flash('that user doesn\'t exist', 'warning')
-
-            # user = db.session.execute(db.select(User).filter_by(username=user_name))   ---> newer syntax, I still prefer the old!
-    return render_template('login.html', form=form)
+    user = User.query.filter_by(username=user_name).first()
+    if user and check_password_hash(user.password, password):
+        login_user(user)
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"message": "Invalid credentials"}), 401
 
 @auth.route('/logout')
 def logout():
-    flash('you\'re logged out, have a great day!', 'secondary')
     logout_user()
-    return redirect(url_for('http://localhost:5173/'))
-
+    return redirect('http://localhost:5173/')
